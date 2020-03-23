06d381f9d32244c17081e3428d8d54d27615fee7
TJ <talhajamil114@gmail.com>
2020-03-23 14:41:47 +0500

    Implementing ngxs

--- ShopManagement-SPA/src/app/_views/login/login.module.ts
+++ ShopManagement-SPA/src/app/_views/login/login.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
 
-import { LoginComponent } from './login.component';
-import { AngularMaterialModule } from '../../_shared/AngularMaterial.module';
-import { LoginRouting } from './login.routing';
+import {LoginComponent} from './login.component';
+import {AngularMaterialModule} from '../../_shared/AngularMaterial.module';
+import {LoginRouting} from './login.routing';
+import {NgxsModule} from '@ngxs/store';
+import {LoginState} from './state/login.state';
 
 @NgModule({
-   imports: [FormsModule, HttpClientModule, CommonModule, LoginRouting, AngularMaterialModule],
+   imports: [
+      FormsModule,
+      HttpClientModule,
+      CommonModule,
+      LoginRouting,
+      AngularMaterialModule,
+      NgxsModule.forFeature([LoginState]),
+   ],
    declarations: [LoginComponent],
    exports: [],
 })
